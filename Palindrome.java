ad3ff378777abed8f33312591731a1b8f84ba7b4
vishnu7899 <119359369+vishnu7899@users.noreply.github.com>
2024-07-07 09:19:12 +0530

    First Commit

---
+++ Palindrome.java
@@ -0,0 +1,23 @@
+
+public class Palindrome {
+//logic starts
+    public boolean isPalindrome(int x) {
+        int temp=x;
+        int rev=0;
+        if(x<0){
+            return false;
+        }
+        while(x!=0){
+            int lastdigit=x%10;
+            rev=(rev*10)+lastdigit;
+            x=x/10;
+        }
+        return (rev==temp);
+    }
+    //logic ends
+    public static void main(String[] args) {        //not needed in solving a problem in leetcode.
+        Palindrome obj=new Palindrome();
+        System.out.println(obj.isPalindrome(11211));
+
+}
+}
---
+++ Reverse.java
@@ -0,0 +1,25 @@
+public class Reverse {
+    public int reverse(int x) {
+        int num = Math.abs(x);  // Original number ka absolute value nikala
+        
+        int rev = 0;  // Reversed number
+        
+        while (num != 0) {
+            int ld = num % 10;  
+            
+            // Overflow check
+            if (rev > (Integer.MAX_VALUE - ld) / 10) {
+                return 0;  // Agar overflow hua, toh 0 return kardo
+            }
+            
+            rev = rev * 10 + ld;  // Reverse mein digit ko add kiya
+            num = num / 10;  // Last digit hata diya, next iteration ke liye
+        }
+        
+        return (x < 0) ? (-rev) : rev;  // Original number ke sign ke hisaab se result diya
+    }
+    public static void main(String[] args) {
+        Reverse r = new Reverse();
+        System.out.println(r.reverse(284));
+}
+}
